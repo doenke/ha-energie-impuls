3b1ed9bda057f571176e95c1092d6ecef6a10f3e
doenke <soenke@soenkejacobs.de>
2025-06-02 14:49:05 +0200

    Update const.py

--- const.py
+++ const.py
@@ -2,3 +2,6 @@ DOMAIN = "energie_impuls"
 LOGIN_URL = "https://energie-impuls.site/api/auth/login"
 DATA_URL = "https://energie-impuls.site/api/c/devices/flow/"
 WALLBOX_URL = "https://energie-impuls.site/api/c/devices/wallbox/?nested=true"
+
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"

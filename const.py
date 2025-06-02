e020ccdc4e6c33668eaa487500fe26eebdcad40a
doenke <soenke@soenkejacobs.de>
2025-06-02 14:45:37 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,4 @@
+DOMAIN = "energie_impuls"
+LOGIN_URL = "https://energie-impuls.site/api/auth/login"
+DATA_URL = "https://energie-impuls.site/api/c/devices/flow/"
+WALLBOX_URL = "https://energie-impuls.site/api/c/devices/wallbox/?nested=true"

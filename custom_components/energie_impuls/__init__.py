5f7835b751b0409770fd2e4fd6492821f851f2a6
doenke <soenke@soenkejacobs.de>
2025-06-06 10:32:22 +0200

    Update __init__.py

--- custom_components/energie_impuls/__init__.py
+++ custom_components/energie_impuls/__init__.py
@@ -4,9 +4,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    await hass.config_entries.async_forward_entry_setup(entry, "switch")
-    await hass.config_entries.async_forward_entry_setup(entry, "number")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch", "number"])
     return True
 
 

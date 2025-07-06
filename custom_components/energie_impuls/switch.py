bd090c4a70cf557033573be327493a76ed01e966
doenke <soenke@soenkejacobs.de>
2025-07-06 22:47:11 +0200

    Aktualisieren von switch.py

--- custom_components/energie_impuls/switch.py
+++ custom_components/energie_impuls/switch.py
@@ -43,7 +43,7 @@ class EnergieImpulsSwitch(SwitchEntity):
 
     @property
     def device_info(self):
-         {
+         return {
             "identifiers": {("energie_impuls_wallbox_location", f"wallbox_{self._device_id}")},
             "name": "Energie Impuls Wallbox",
             "manufacturer": "Energie Impuls",

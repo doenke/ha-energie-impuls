a136082aff5c8ea2332f12339b0a8537998f7f56
doenke <soenke@soenkejacobs.de>
2025-07-06 18:11:59 +0200

    Update __init__.py

--- custom_components/energie_impuls/__init__.py
+++ custom_components/energie_impuls/__init__.py
@@ -1,10 +1,13 @@
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .automation_vollladen import VollladenAutomatik
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch", "number"])
+    hass.data[DOMAIN]["automatik"] = VollladenAutomatik(hass)
+    await hass.data[DOMAIN]["automatik"].async_initialize()
     return True
 
 

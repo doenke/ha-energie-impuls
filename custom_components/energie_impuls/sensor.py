f6cfde286bcc953979ad39e18f8d445c635658c5
doenke <soenke@soenkejacobs.de>
2025-07-06 18:41:40 +0200

    Update sensor.py

--- custom_components/energie_impuls/sensor.py
+++ custom_components/energie_impuls/sensor.py
@@ -7,11 +7,11 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 SENSOR_TYPES = {
-    "pv": {"name": "PV-Erzeugung", "unit": "kW"},
-    "to_grid": {"name": "Netzeinspeisung", "unit": "kW"},
-    "to_battery": {"name": "Batterie-Ladung", "unit": "kW"},
-    "household": {"name": "Haushalt", "unit": "kW"},
-    "battery_soc": {"name": "Batterie Ladezustand", "unit": "%"}
+    "pv": {"name": "PV-Erzeugung", "unit": "kW","icon": "mdi:solar-power-variant"},
+    "to_grid": {"name": "Netzeinspeisung", "unit": "kW","icon": "mdi:transmission-tower"},
+    "to_battery": {"name": "Batterie-Ladung", "unit": "kW","icon": "mdi:battery-charging"},
+    "household": {"name": "Haushalt", "unit": "kW","icon": "mdi:home"},
+    "battery_soc": {"name": "Batterie Ladezustand", "unit": "%","icon": "mdi:battery-charging"}
 }
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -23,12 +23,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     # Zusätzliche Wallbox-Sensoren
     sensors.extend([
-        WallboxSensor(session, "Wallbox Modus", "wallbox_mode_str", lambda d: d["_state"]["mode_str"]),
+        WallboxSensor(session, "Wallbox Modus", "wallbox_mode_str", lambda d: d["_state"]["mode_str"],None,"mdi:ev-pug-type2"),
         WallboxSensor(session, "Wallbox Moduscode", "wallbox_mode", lambda d: d["_state"]["mode"]),
-        WallboxSensor(session, "Wallbox Verbrauch", "wallbox_consumption", lambda d: d["_state"]["consumption"], "kW"),
+        WallboxSensor(session, "Wallbox Verbrauch", "wallbox_consumption", lambda d: d["_state"]["consumption"], "kW","mdi:ev-station"),
         WallboxSensor(session, "Wallbox Zeitstempel", "wallbox_timestamp", lambda d: d["_state"]["timestamp"]),
         WallboxSensor(session, "Wallbox Seit Modus aktiv", "wallbox_mode_since", lambda d: d["_state"]["mode_since"]),
-        # WallboxSensor(session, "Wallbox Hybridstrom", "wallbox_hybrid_charging", lambda d: d["_set_point"]["hybrid_charging_current"], "A"),
         WallboxSensor(session, "Wallbox Name", "wallbox_name", lambda d: d["name"]),
         WallboxSensor(session, "Wallbox Standort-ID", "wallbox_location", lambda d: d["location"]),
     ])
@@ -103,6 +102,7 @@ class EnergieImpulsSensor(Entity):
         self._attr_name = f"Energie Impuls {SENSOR_TYPES[key]['name']}"
         self._attr_unique_id = f"energie_impuls_{key}"
         self._attr_unit_of_measurement = SENSOR_TYPES[key].get("unit")
+        self._attr_icon = "SENSOR_TYPES[key].get("icon")
         self._state = None
 
     def update(self):
@@ -118,12 +118,14 @@ class EnergieImpulsSensor(Entity):
         return self._state
 
 class WallboxSensor(Entity):
-    def __init__(self, session, name, unique_id, extract_func, unit=None):
+    def __init__(self, session, name, unique_id, extract_func, unit=None, icon=none):
         self._session = session
         self._extract_func = extract_func
         self._attr_name = f"Energie Impuls {name}"
         self._attr_unique_id = f"energie_impuls_{unique_id}"
         self._attr_unit_of_measurement = unit
+        self._attr_icon = icon
+        
         self._state = None
 
     def update(self):

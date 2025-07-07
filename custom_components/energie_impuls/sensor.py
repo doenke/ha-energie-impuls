402b5c8eadf2ba93ce60104d96ddb6aaffcd3b8f
doenke <soenke@soenkejacobs.de>
2025-07-07 07:25:14 +0200

    Update sensor.py

--- custom_components/energie_impuls/sensor.py
+++ custom_components/energie_impuls/sensor.py
@@ -141,16 +141,6 @@ class WallboxSensor(Entity):
         
         self._state = None
 
-        # Dynamischer Gerätename einmalig abrufen
-        try:
-            data = self._session.get_wallbox_data()
-            self._device_name = data.get("wallbox_name", "Wallbox")
-            self._device_id = str(data.get("wallbox_location", "unknown"))
-        except Exception as e:
-            _LOGGER.warning(f"Konnte Wallbox-Name nicht abrufen: {e}")
-            self._device_name = "Wallbox"
-            self._device_id = "unknown"
-
     def update(self):
         try:
             data = self._session.get_wallbox_data()
@@ -166,10 +156,10 @@ class WallboxSensor(Entity):
     @property
     def device_info(self):
          return {
-            "identifiers": {("energie_impuls_wallbox_location", f"wallbox_{self._device_id}")},
+            "identifiers": {(DOMAIN, f"wallbox_{hass.data[DOMAIN]["wb_device_id"]}")},
             "name": "Energie Impuls Wallbox",
-            "manufacturer": "Energie Impuls",
-            "model": self._device_name,
+            "manufacturer": "ABB",
+            "model": hass.data[DOMAIN]["wb_device_name"],
             "configuration_url": "https://energie-impuls.site",
         }
         

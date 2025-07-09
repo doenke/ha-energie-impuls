854f0d9ec89c428ef0ca228bcf54ff651ea1f837
doenke <soenke@soenkejacobs.de>
2025-07-09 13:52:43 +0200

    Create devices.py

---
+++ custom_components/energie_impuls/devices.py
@@ -0,0 +1,15 @@
+from .const import DOMAIN,CONF_WB_DEVICE_NAME, CONF_WB_DEVICE_ID
+
+class EnergieImpulsWallboxDevice():
+    def __init__(self, hass):
+        self.hass=hass
+
+    @property
+    def info(self):
+         return {
+            "identifiers": {(DOMAIN, f"wallbox_{hass.data[DOMAIN][CONF_WB_DEVICE_ID]}")},
+            "name": "Energie Impuls Wallbox",
+            "manufacturer": "ABB",
+            "model": hass.data[DOMAIN][CONF_WB_DEVICE_NAME ],
+            "configuration_url": "https://energie-impuls.site",
+        }

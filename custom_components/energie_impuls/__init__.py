56e720c8c317897aee6a9e68f3dfde32fff4ae44
doenke <soenke@soenkejacobs.de>
2025-07-09 13:11:51 +0200

    Update __init__.py

--- custom_components/energie_impuls/__init__.py
+++ custom_components/energie_impuls/__init__.py
@@ -3,7 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .automation_nightfull import VollladenAutomatik
 from .sensor import EnergyImpulsSession
-from .const import DOMAIN, DEFAULT_WALLBOX_NAME, DEFAULT_WALLBOX_ID
+from .const import DOMAIN, DEFAULT_WALLBOX_NAME, DEFAULT_WALLBOX_ID, CONF_USERNAME, CONF_PASSWORD, CONF_WB_DEVICE_NAME, CONF_WB_DEVICE_ID
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if DOMAIN not in hass.data:
@@ -22,8 +22,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         wb_device_name = DEFAULT_WALLBOX_NAME
         wb_device_id = DEFAULT_WALLBOX_ID
 
-    hass.data[DOMAIN]["wb_device_name"] = wb_device_name
-    hass.data[DOMAIN]["wb_device_id"] = wb_device_id
+    hass.data[DOMAIN][CONF_WB_DEVICE_NAME] = wb_device_name
+    hass.data[DOMAIN][CONF_WB_DEVICE_ID] = wb_device_id
     hass.data[DOMAIN]["session"] = session
     
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "switch", "number", "select"])

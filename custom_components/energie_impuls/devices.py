e52e0462cb6616b557b249576e29b6bbd54a1f45
doenke <soenke@soenkejacobs.de>
2025-07-09 14:22:38 +0200

    Update devices.py

--- custom_components/energie_impuls/devices.py
+++ custom_components/energie_impuls/devices.py
@@ -13,3 +13,20 @@ class EnergieImpulsWallboxDevice():
             "model": self.hass.data[DOMAIN][CONF_WB_DEVICE_NAME ],
             "configuration_url": "https://energie-impuls.site",
         }
+
+
+
+
+class EnergieImpulsWallboxDevice():
+    def __init__(self, hass):
+        self.hass=hass
+
+    @property
+    def device_info(self):
+         return {
+            "identifiers": {(DOMAIN, f"wallbox_{self.hass.data[DOMAIN][CONF_WB_DEVICE_ID]}")},
+            "name": "Energie Impuls Wallbox",
+            "manufacturer": "ABB",
+            "model": self.hass.data[DOMAIN]["wb_device_name"],
+            "configuration_url": "https://energie-impuls.site",
+        }

21c2754977f09ad712ad0f2f7b91e8ae6364bc80
doenke <soenke@soenkejacobs.de>
2025-07-14 06:23:05 +0200

    Update sensor.py

--- custom_components/energie_impuls/sensor.py
+++ custom_components/energie_impuls/sensor.py
@@ -1,4 +1,5 @@
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.restore_state import RestoreEntity
 from .const import DOMAIN
 from .api import EnergyImpulsSession
@@ -17,8 +18,8 @@ SENSOR_TYPES = {
 
 async def async_setup_entry(hass, entry, async_add_entities):
     session = hass.data[DOMAIN]["session"]
-
-    sensors = [EnergieImpulsSensor(hass, session, key) for key in SENSOR_TYPES]
+    energie_coordinator = hass.data[DOMAIN]["coordinator_energie"]
+    sensors = [EnergieImpulsSensor(hass, energie_coordinator, key) for key in SENSOR_TYPES]
 
     sensors.extend([
         WallboxSensor(hass, session, "Wallbox Modus", "wallbox_mode_str", lambda d: d["_state"]["mode_str"], None, "mdi:ev-plug-type2"),
@@ -33,10 +34,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities(sensors, update_before_add=True)
 
 
-class EnergieImpulsSensor(Entity):
-    def __init__(self, hass, session, key):
+class EnergieImpulsSensor(CoordinatorEntity,Entity):
+    def __init__(self, hass, coordinator, key):
         self.hass = hass
-        self._session = session
+        super().__init__(coordinator)
+        self.coordinator = coordinator
         self._key = key
         self._attr_name = SENSOR_TYPES[key]['name']
         self._attr_unique_id = f"energie_impuls_{key}"
@@ -44,17 +46,13 @@ class EnergieImpulsSensor(Entity):
         self._attr_icon = SENSOR_TYPES[key].get("icon")
         self._state = None
 
-    async def async_update(self):
-        try:
-            data = await self._session.async_get_data()
-            self._state = data["flow"].get(self._key) or data["state"].get(self._key)
-        except Exception as e:
-            _LOGGER.error(f"Sensorfehler ({self._key}): {e}")
-            self._state = None
-
     @property
     def state(self):
-        return self._state
+        data = self.coordinator.data
+        if data is None:
+            return None
+        return data.get("flow", {}).get(self._key) or data.get("state", {}).get(self._key)
+
 
     @property
     def device_info(self):

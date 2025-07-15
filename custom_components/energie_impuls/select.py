faa8fc99d76d1cf99c3597b3d7d72195de5594e9
doenke <soenke@soenkejacobs.de>
2025-07-15 13:50:04 +0200

    Update select.py

--- custom_components/energie_impuls/select.py
+++ custom_components/energie_impuls/select.py
@@ -1,7 +1,8 @@
 from homeassistant.components.select import SelectEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .devices import EnergieImpulsWallboxDevice, EnergieImpulsDevice
 from .const import DOMAIN
-from .api import EnergyImpulsSession
+
 import logging
 
 _LOGGER = logging.getLogger(__name__)
@@ -10,37 +11,52 @@ WALLBOX_MODES = [
     "Schnellladen",
     "reines Überschussladen",
     "Hybridladen",
-    "Nicht laden"
+    "Nicht laden",
+    "Fehler"
 ]
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    session =  hass.data[DOMAIN]["session"]
-    async_add_entities([WallboxModeSelect(hass,session)], update_before_add=True)
+    wallbox_coordinator = hass.data[DOMAIN]["coordinator_wallbox"]
+    async_add_entities([WallboxModeSelect(hass,wallbox_coordinator)], update_before_add=True)
 
-class WallboxModeSelect(SelectEntity):
-    def __init__(self, hass, session):
-        self._session = session
+class WallboxModeSelect(CoordinatorEntity, SelectEntity):
+    def __init__(self, hass, coordinator):
+        self.coordinator = coordinator
         self._attr_name = "Wallbox Lademodus"
         self._attr_unique_id = "energie_impuls_wallbox_mode"
         self._attr_options = WALLBOX_MODES
         self._attr_current_option = None
         self.hass = hass
-    #def update(self):
-    #    try:
-    #        #data = self._session.get_wallbox_data()
-    #        # Mapping der internen Modus-Codes zu benutzerfreundlichen Labels
-    #        mode_code = data["_state"].get("mode")
-    #        self._attr_current_option = self._map_code_to_label(mode_code)
-    #    except Exception as e:
-    #        _LOGGER.error(f"Fehler beim Lesen des Wallbox-Modus: {e}")
+    
+    async def async_update(self):
+        await self.coordinator.async_request_refresh()
 
-    def select_option(self, option: str):
-        _LOGGER.info(f"Lademodus wird gesetzt auf: {option}")
-        # Optional: sende API-Aufruf zur Modusumschaltung
-        # Hier z. B. Session PUT auf /setpoint mit gewünschtem Modus
-        self._attr_current_option = option
+    @property
+    def current_option(self)
+        try:
+            if self.coordinator.data["_set_point"]["locked"] == True:
+                return 0
+            if self.coordinator.data["_set_point"]["surplus_charging"] == True:
+                if self.coordinator.data["_set_point"]get("hybrid_charging_current", 0) == 0:
+                    return 2
+                else:
+                    return 3
+            else:
+                return 1
+        except:
+            return 4
         
+    def select_option(self, option: str):
+        if option == "1":
+        elif option == "2":
+        elif option == "3":
+        elif option == "4":
 
+        payload = {
+                "hybrid_charging_current": None if int(value) == 0 else int(value)
+            }
+
+            response = await self.coordinator.session.async_put_wallbox_setpoint(payload)
     def _map_code_to_label(self, code):
         # Optional: Mapping von internen Werten auf Labels
         mapping = {
@@ -48,6 +64,7 @@ class WallboxModeSelect(SelectEntity):
             1: "Schnellladen",
             2: "reines Überschussladen",
             3: "Hybridladen"
+            4: "Fehler"
         }
         return mapping.get(code, "Unbekannt")
     @property

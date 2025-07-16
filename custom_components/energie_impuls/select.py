b7d423a9074a8aad0d6dddf3d83344fc98faada9
doenke <soenke@soenkejacobs.de>
2025-07-16 11:27:42 +0200

    Update select.py

--- custom_components/energie_impuls/select.py
+++ custom_components/energie_impuls/select.py
@@ -1,4 +1,6 @@
 from homeassistant.components.select import SelectEntity
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .devices import EnergieImpulsWallboxDevice, EnergieImpulsDevice
 from .const import DOMAIN
@@ -30,6 +32,14 @@ NICHTLADEN_JSON =  {
             }
 ERROR = "Fehler"
 
+# Automatiklabel
+AM_SCHNELLLADEN = SCHNELLLADEN
+AM_UEBERSCHUSS = UEBERSCHUSS
+AM_HYBRIDAUTOMATIK = "Hybridautomatik"
+AM_UEBERSCHUSS_NACHT = "Überschuss, über Nacht voll"
+AM_HYBRIDAUTOMATIK_NACHT = "Hybrid, über Nacht voll"
+AM_MANUAL = "Manuell"
+
 WALLBOX_MODES = [
     SCHNELLLADEN,
     UEBERSCHUSS,
@@ -38,9 +48,109 @@ WALLBOX_MODES = [
     ERROR
 ]
 
+AUTOMATIC_MODES = [
+  AM_SCHNELLLADEN,
+  AM_UEBERSCHUSS,
+  AM_HYBRIDAUTOMATIK,
+  AM_UEBERSCHUSS_NACHT,
+  AM_HYBRIDAUTOMATIK_NACHT,
+  AM_MANUAL
+]
+
+MIN_HYBRID = 1.5
+MIN_HYBRID_MINUTES = 10
+
+
 async def async_setup_entry(hass, entry, async_add_entities):
     wallbox_coordinator = hass.data[DOMAIN]["coordinator_wallbox"]
     async_add_entities([WallboxModeSelect(hass,wallbox_coordinator)], update_before_add=True)
+    async_add_entities([AutomaticModeActiveSwitch(hass,wallbox_coordinator)], update_before_add=True)
+
+
+class WallboxAutomaticModeSelect(CoordinatorEntity, RestoreEntity, SelectEntity):
+    def __init__(self, hass, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Wallbox Automatikmodus"
+        self._attr_unique_id = "energie_impuls_automatic_mode"
+        self._attr_options = AUTOMATIC_MODES
+        self.hass = hass
+        self._key = AM_MANUAL
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        last_state = await self.async_get_last_state()
+        if last_state:
+            self._attr_current_option = last_state.state
+            _LOGGER.debug(f"[{self._key}] Wiederhergestellter Zustand: {self._attr_current_option}")
+        else:
+            self._attr_current_option = AM_MANUAL  # Optionaler Fallback
+            _LOGGER.debug(f"[{self._key}] Kein gespeicherter Zustand gefunden – Manuell gesetzt")
+        self.async_write_ha_state()
+
+    @property
+    def current_option(self):
+        return self._attr_current_option
+        
+    async def async_select_option(self, option: str):
+        if option == AM_SCHNELLLADEN:
+            payload = SCHNELLLADEN_JSON
+        elif option == AM_UEBERSCHUSS:
+            payload = UEBERSCHUSS_JSON
+        elif option == AM_HYBRIDAUTOMATIK:
+            payload = HYBRID_JSON
+        elif option == AM_UEBERSCHUSS_NACHT:
+            payload = NICHTLADEN_JSON
+        else:
+          return
+
+        try:
+            await self.coordinator.async_set_wallbox_mode(payload)
+            self._attr_current_option = option
+            self.async_write_ha_state()
+        except Exception as e:
+           _LOGGER.error(f"Fehler beim Setzen des Lademodus: {e}")
+           
+    @property
+    def device_info(self):
+         return EnergieImpulsWallboxDevice(self.hass).device_info
+
+class AutomaticModeActiveSwitch(RestoreEntity, SwitchEntity):
+    def __init__(self, hass):
+        self.hass = hass
+        self._state = False
+        self._attr_name = "Automatik"
+        self._attr_unique_id = "energie_impuls_automatic_status"
+        self._attr_icon = "mdi:weather-night"
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        old_state = await self.async_get_last_state()
+        if old_state is not None:
+            self._state = old_state.state == "on"
+            _LOGGER.info(f"Zustand wiederhergestellt: {self._state}")
+
+    @property
+    def is_on(self):
+        return self._state
+
+    @property
+    def device_info(self):
+        return EnergieImpulsWallboxDevice(self.hass).device_info
+
+    async def async_turn_on(self, **kwargs):
+        _LOGGER.info("Ladeautomatik aktiviert")
+        self._state = True
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        _LOGGER.info("Ladeautomatik deaktiviert")
+        self._state = False
+        self.async_write_ha_state()
+
+    async def async_update(self):
+        pass
+
+
 
 class WallboxModeSelect(CoordinatorEntity, SelectEntity):
     def __init__(self, hass, coordinator):

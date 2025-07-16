f4bf9cc149fbc5020c39f718a6a2c3b4cd245ed6
doenke <soenke@soenkejacobs.de>
2025-07-16 13:07:15 +0200

    Update const.py

--- custom_components/energie_impuls/const.py
+++ custom_components/energie_impuls/const.py
@@ -12,3 +12,51 @@ CONF_AUTO_SWITCH_ENTITY = "auto_switch_entity"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_SESSION = "session"
+
+SCHNELLLADEN = "Schnellladen"
+SCHNELLLADEN_JSON = {
+                "locked": False,
+                "surplus_charging": False
+                }
+UEBERSCHUSS = "reines Überschussladen"
+UEBERSCHUSS_JSON = {
+                "locked": False,
+                "surplus_charging": True,
+                "hybrid_charging_current": None
+                }
+HYBRID = "Hybridladen"
+HYBRID_JSON =  {
+                "locked": False,
+                "surplus_charging": True,
+                "hybrid_charging_current": 6
+                }
+NICHTLADEN = "nicht laden"
+NICHTLADEN_JSON =  {
+            "locked": True
+            }
+ERROR = "Fehler"
+
+# Automatiklabel
+AM_SCHNELLLADEN = SCHNELLLADEN
+AM_UEBERSCHUSS = UEBERSCHUSS
+AM_HYBRIDAUTOMATIK = "Hybridautomatik"
+AM_UEBERSCHUSS_NACHT = "Überschuss, über Nacht voll"
+AM_HYBRIDAUTOMATIK_NACHT = "Hybrid, über Nacht voll"
+AM_MANUAL = "Manuell"
+
+WALLBOX_MODES = [
+    SCHNELLLADEN,
+    UEBERSCHUSS,
+    HYBRID,
+    NICHTLADEN,
+    ERROR
+]
+
+AUTOMATIC_MODES = [
+  AM_SCHNELLLADEN,
+  AM_UEBERSCHUSS,
+  AM_HYBRIDAUTOMATIK,
+  AM_UEBERSCHUSS_NACHT,
+  AM_HYBRIDAUTOMATIK_NACHT,
+  AM_MANUAL
+]

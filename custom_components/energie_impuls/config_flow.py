69918c7e469854ccadf08f33b72c37ac23baea08
doenke <soenke@soenkejacobs.de>
2025-07-17 09:02:44 +0200

    Update config_flow.py

--- custom_components/energie_impuls/config_flow.py
+++ custom_components/energie_impuls/config_flow.py
@@ -1,7 +1,9 @@
-
 from homeassistant import config_entries
 import voluptuous as vol
 from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD
+
+DEFAULT_MIN_HYBRID = 1.5
+DEFAULT_MIN_HYBRID_MINUTES = 10
 
 class EnergieImpulsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
@@ -15,3 +17,41 @@ class EnergieImpulsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    def async_get_options_flow(self, config_entry):
+        return EnergieImpulsOptionsFlowHandler(config_entry)
+
+
+class EnergieImpulsOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            # Speichern und reload anstoßen
+            self.hass.config_entries.async_update_entry(
+                self.config_entry,
+                data={
+                    CONF_USERNAME: user_input[CONF_USERNAME],
+                    CONF_PASSWORD: user_input[CONF_PASSWORD],
+                },
+                options={
+                    "min_hybrid": user_input["min_hybrid"],
+                    "min_hybrid_minutes": user_input["min_hybrid_minutes"],
+                },
+            )
+            await self.hass.config_entries.async_reload(self.config_entry.entry_id)
+            return self.async_create_entry(title="", data={})
+
+        current_data = self.config_entry.data
+        current_options = self.config_entry.options
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(CONF_USERNAME, default=current_data.get(CONF_USERNAME, "")): str,
+                vol.Required(CONF_PASSWORD, default=current_data.get(CONF_PASSWORD, "")): str,
+                vol.Required("min_hybrid", default=current_options.get("min_hybrid", DEFAULT_MIN_HYBRID)): float,
+                vol.Required("min_hybrid_minutes", default=current_options.get("min_hybrid_minutes", DEFAULT_MIN_HYBRID_MINUTES)): int,
+            }),
+        )

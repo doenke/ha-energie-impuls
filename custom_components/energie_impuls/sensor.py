100ec08346c8f482f68b37cda629da94d912d613
doenke <soenke@soenkejacobs.de>
2025-07-17 10:10:15 +0200

    Update sensor.py

--- custom_components/energie_impuls/sensor.py
+++ custom_components/energie_impuls/sensor.py
@@ -28,7 +28,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Zeitstempel", "wallbox_timestamp", lambda d: d["_state"]["timestamp"]),
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Seit Modus aktiv", "wallbox_mode_since", lambda d: d["_state"]["mode_since"]),
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Standort-ID", "wallbox_location", lambda d: d["location"]),
-        VollladenStatusSensor(hass),
+        ShortWallboxModeSensor(hass, wallbox_coordinator, "KNX Wallbox Modus", "wallbox_mode_knx", lambda d: d["_state"]["mode_str"], None, "mdi:cog-outline"),
     ])
 
     async_add_entities(sensors, update_before_add=True)
@@ -67,7 +67,6 @@ class WallboxSensor(CoordinatorEntity,Entity):
         self._extract_func = extract_func
         self._attr_name = name
         self._attr_unique_id = f"energie_impuls_{unique_id}"
-        self._attr_unit_of_measurement = unit
         self._attr_icon = icon
         self._state = None
 
@@ -81,36 +80,11 @@ class WallboxSensor(CoordinatorEntity,Entity):
         return EnergieImpulsWallboxDevice(self.hass).device_info
 
 
-class VollladenStatusSensor(RestoreEntity, Entity):
-    def __init__(self, hass):
-        self.hass = hass
-        self._attr_name = "Vollladen über Nacht jetzt aktiv"
-        self._attr_unique_id = "vollladen_status_sensor"
-        self._state = False
-        self.entity_id = "binary_sensor.vollladen_uber_nacht_status"
-        self._icon = "mdi:weather-night"
-
-    async def async_added_to_hass(self):
-        await super().async_added_to_hass()
-        old_state = await self.async_get_last_state()
-        self._state = old_state and old_state.state == "on"
-
-    @property
-    def is_on(self):
-        return self._state
-
+class ShortWallboxModeSensor(WallboxSensor):
     @property
     def state(self):
-        return "on" if self._state else "off"
-
-    @property
-    def device_class(self):
-        return "running"
-
-    async def async_turn_on(self):
-        self._state = True
-        self.async_write_ha_state()
-
-    async def async_turn_off(self):
-        self._state = False
-        self.async_write_ha_state()
+        value = self._extract_func(self.coordinator.data)
+        if isinstance(value, str):
+            value = value.replace("Fahrzeug", "").strip()
+        self._state = value
+        return 0 if self._state is None else self._state

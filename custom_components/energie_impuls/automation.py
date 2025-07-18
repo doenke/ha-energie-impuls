d1f5fe63a5e74cde5b107d36dd41590c9177e520
doenke <soenke@soenkejacobs.de>
2025-07-18 08:19:50 +0200

    Update automation.py

--- custom_components/energie_impuls/automation.py
+++ custom_components/energie_impuls/automation.py
@@ -96,24 +96,7 @@ class AutomatikBase:
      def isCurrentOption(self):
           return self.mode_entity.current_option == self.mode
 
-     async def _set_mode(self, mode_name):
-        payloads = {
-            HYBRID: HYBRID_JSON,
-            NICHTLADEN: NICHTLADEN_JSON,
-            SCHNELLLADEN: SCHNELLLADEN_JSON,
-            UEBERSCHUSS: UEBERSCHUSS_JSON
-        }
-        payload = payloads.get(mode_name)
-        if not payload:
-            return
-
-        try:
-            await self.wallbox_coordinator.async_set_wallbox_mode(payload)
-            #self.mode_entity.async_write_ha_state()
-            _LOGGER.info(f"[{self.mode}] Wallbox-Modus gesetzt auf: {mode_name}")
-        except Exception as e:
-            _LOGGER.error(f"[HybridAutomatikController] Fehler beim Setzen des Modus {mode_name}: {e}")
-
+    
 class AutomatikControllerPVGrenze(AutomatikBase):
     def __init__(self, hass, entry, mode, offaction, onaction):
          super().__init__(hass, mode, entry)

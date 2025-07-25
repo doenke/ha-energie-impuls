3ff3a4a08cd4b656515d97081258f8a1a114b67a
doenke <soenke@soenkejacobs.de>
2025-07-25 13:51:25 +0200

    Update sensor.py

--- custom_components/energie_impuls/sensor.py
+++ custom_components/energie_impuls/sensor.py
@@ -1,5 +1,10 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorStateClass,
+)
+
 from .const import DOMAIN
 from .api import EnergyImpulsSession
 from .devices import EnergieImpulsWallboxDeviceInfoMixin, EnergieImpulsDeviceInfoMixin
@@ -10,20 +15,19 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, entry, async_add_entities):
     energie_coordinator = hass.data[DOMAIN]["coordinator_energie"]
     wallbox_coordinator = hass.data[DOMAIN]["coordinator_wallbox"]
-    sensors = 
-    [
-        EnergieImpulsSensor(hass, energie_coordinator,"PV-Erzeugung", "pv", "kW","mdi:solar-power-variant","power"), 
-        EnergieImpulsSensor(hass, energie_coordinator,"Netzeinspeisung", "to_grid", "kW","mdi:transmission-tower","power"), 
-        EnergieImpulsSensor(hass, energie_coordinator,"Batterie-Ladung", "to_battery", "kW","mdi:battery-charging","power"), 
-        EnergieImpulsSensor(hass, energie_coordinator,"Haushalt", "household", "kW","mdi:home","power"), 
-        EnergieImpulsSensor(hass, energie_coordinator,"Batterie Ladezustand", "battery_soc", "%","mdi:battery-charging","battery"), 
-        WallboxSensor(hass, wallbox_coordinator, "Wallbox Modus", "wallbox_mode_str", lambda d: d["_state"]["mode_str"], None, "mdi:ev-plug-type2","enum"),
-        WallboxSensor(hass, wallbox_coordinator, "Wallbox Moduscode", "wallbox_mode", lambda d: d["_state"]["mode"],"enum"),
-        WallboxSensor(hass, wallbox_coordinator, "Wallbox Verbrauch", "wallbox_consumption", lambda d: d["_state"]["consumption"], "kW", "mdi:ev-station","power"),
+    sensors = [
+        EnergieImpulsSensor(hass, energie_coordinator,"PV-Erzeugung", "pv", "kW","mdi:solar-power-variant",SensorDeviceClass.POWER), 
+        EnergieImpulsSensor(hass, energie_coordinator,"Netzeinspeisung", "to_grid", "kW","mdi:transmission-tower",SensorDeviceClass.POWER), 
+        EnergieImpulsSensor(hass, energie_coordinator,"Batterie-Ladung", "to_battery", "kW","mdi:battery-charging",SensorDeviceClass.POWER), 
+        EnergieImpulsSensor(hass, energie_coordinator,"Haushalt", "household", "kW","mdi:home",SensorDeviceClass.POWER), 
+        EnergieImpulsSensor(hass, energie_coordinator,"Batterie Ladezustand", "battery_soc", "%","mdi:battery-charging",SensorDeviceClass.BATTERY), 
+        WallboxSensor(hass, wallbox_coordinator, "Wallbox Modus", "wallbox_mode_str", lambda d: d["_state"]["mode_str"], None, "mdi:ev-plug-type2",SensorDeviceClass.ENUM),
+        WallboxSensor(hass, wallbox_coordinator, "Wallbox Moduscode", "wallbox_mode", lambda d: d["_state"]["mode"],SensorDeviceClass.ENUM),
+        WallboxSensor(hass, wallbox_coordinator, "Wallbox Verbrauch", "wallbox_consumption", lambda d: d["_state"]["consumption"], "kW", "mdi:ev-station",SensorDeviceClass.POWER),
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Zeitstempel", "wallbox_timestamp", lambda d: d["_state"]["timestamp"]),
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Seit Modus aktiv", "wallbox_mode_since", lambda d: d["_state"]["mode_since"]),
         #WallboxSensor(hass, wallbox_coordinator, "Wallbox Standort-ID", "wallbox_location", lambda d: d["location"]),
-        ShortWallboxModeSensor(hass, wallbox_coordinator, "KNX Wallbox Modus", "wallbox_mode_knx", lambda d: d["_state"]["mode_str"], None, "mdi:cog-outline","enum"),
+        ShortWallboxModeSensor(hass, wallbox_coordinator, "KNX Wallbox Modus", "wallbox_mode_knx", lambda d: d["_state"]["mode_str"], None, "mdi:cog-outline",SensorDeviceClass.ENUM),
     ]
 
     async_add_entities(sensors, update_before_add=True)
@@ -33,11 +37,13 @@ class EnergieImpulsSensor(EnergieImpulsDeviceInfoMixin,CoordinatorEntity,SensorE
     def __init__(self, hass, coordinator, name, key, unit,icon,device_class):
         self.hass = hass
         super().__init__(coordinator)
+        self._key = key
         self._attr_name = name
         self._attr_unique_id = f"energie_impuls_{key}"
         self._attr_unit_of_measurement = unit
         self._attr_icon = icon
         self._attr_device_class = device_class
+        self._attr_state_class = SensorStateClass.MEASUREMENT
         self._state = None
 
     @property
